650db6d5354fe532df0dc7200bb311ee6c2d49be
YC Lim <lyc950421@gmail.com>
2016-05-28 07:45:44 +0800

    Update Dog.java

--- Lessons/Lesson 13 Factory Pattern/src/Dog.java
+++ Lessons/Lesson 13 Factory Pattern/src/Dog.java
@@ -1,3 +1,7 @@
+/*
+ * Imagine that this class requires lots of configuration
+ * before use, e.g. via constructor parameters
+ */
 public class Dog extends AbstractAnimal implements Animal{
 
 	@Override
